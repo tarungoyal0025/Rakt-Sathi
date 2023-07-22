c8223f7d7fe95a4c1b3832b414445130f3ea5691
tarungoyal0025 <125543703+tarungoyal0025@users.noreply.github.com>
2023-07-22 22:09:30 +0530

    updated app.js for deployment

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 const mongoose = require('mongoose');
 const cookieParser = require("cookie-parser");
+const path = require("path");
 
 const app = express();
 const port = 3177;
@@ -19,12 +20,17 @@ app.use(
 		credentials: true,
 	})
 );
+app.use(express.static(path.join(__dirname,'./client/build')))
 
 
 mongoose.connect(process.env.CONNECT, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }, (e) => {
 	console.log(e ? e : "Connected successfully to database");
 });
 
+app.use('*',function(req,res)
+{
+	res.sendFile(path.join(__dirname,'./client/build/index.html'));
+})
 app.use("/auth", require("./routers/authRouter"));
 app.use("/user", require("./routers/userRouter"));
 app.use("/bank", require("./routers/bankRouter"));
